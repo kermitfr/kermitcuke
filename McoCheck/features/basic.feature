5eaf32449d1344d1dda5e9a9dc44619d4e54ff2f
lofic <louis.coilliot@gmail.com>
2012-10-15 23:58:16 +0200

    First commit

---
+++ McoCheck/features/basic.feature
@@ -0,0 +1,61 @@
+Feature: MCollective check
+  In order to ensure that my MCollective installation works
+  As an Operator 
+  I want to run a quick MCollective check
+
+  Scenario: Mco Ping
+    Given the Agent is rpcutil
+    And the Action is ping
+    And the target is random
+    When I call MCollective
+    Then the StatusMsg is OK
+
+  Scenario: Mco Package
+    Given the Agent is package
+    And the Action is status
+    And the Parameters are
+      | package |
+      | bash    |
+    And the target is random
+    When I call MCollective
+    Then the StatusMsg is OK
+
+  Scenario Outline: Mco Schedule Now
+    Given the Agent is scheduler
+    And the Action is schedule
+    And the Parameters are
+      | agentname | actionname |
+      | rpcutil   | ping       |
+    And the Identity of the target is <nodeid>
+    When I call MCollective
+    Then I should get an hexadecimal jobid
+    And I should get a task in one of those states :
+       | running   |
+       | scheduled |
+       | finished  |
+    Then I should get a good task result within 2 seconds
+
+    Examples: 
+      | nodeid           |
+      | el5.labolinux.fr |
+      | el6.labolinux.fr |
+  
+  Scenario Outline: Mco Schedule in any distant future
+    Given the Agent is scheduler
+    And the Action is schedule
+    And the Parameters are
+      | agentname | actionname | schedtype | schedarg |
+      | rpcutil   | ping       | in        | 600s     |
+    And the Identity of the target is <nodeid>
+    When I call MCollective
+    Then I should get an hexadecimal jobid
+    And I should get a task in one of those states :
+       | scheduled |
+
+    Examples: 
+      | nodeid           |
+      | el5.labolinux.fr |
+      | el6.labolinux.fr |
+
+
+
---
+++ McoCheck/features/step_definitions/basic_steps.rb
@@ -0,0 +1,78 @@
+require 'rspec/expectations' 
+require 'mcollective'
+require 'pp'
+
+MCO_CONFIG = '/etc/mcollective/client.cfg'
+MCO_TIMEOUT = 5
+MCO_DISCOVTMOUT = 1
+MCO_DEBUG = false
+MCO_COLLECTIVE = nil
+
+Given /the Agent is ([A-z_]*)/ do |agent|
+  @agent = agent
+  @mc = MCollective::RPC::Client.new( agent, \
+         :configfile => MCO_CONFIG,
+         :options => {
+             :verbose      => false,
+             :progress_bar => false,
+             :timeout      => MCO_TIMEOUT,
+             :config       => MCO_CONFIG,
+             :filter       => { "agent" => [], "identity" => [] },
+             :collective   => MCO_COLLECTIVE,
+             :disctimeout  => MCO_DISCOVTMOUT } )
+end
+
+Given /the Action is ([A-z_]*)/ do |action|
+  @action = action
+end
+
+Given /the Parameters are/ do |table|
+  myhash = table.hashes.first
+  # convert string keys to symbols
+  @arguments = Hash[myhash.map{|(k,v)| [k.to_sym,v]}]
+end
+
+Given /I wait (\d+) seconds for the task to finish$/ do |t|
+  @delay = t.to_i 
+end
+
+Given /the target is random/ do
+  @mc.limit_targets = 1
+  @mc.limit_method = :random
+end
+
+Given /the Identity of the target is ([A-z0-9\.]*)/ do |id|
+  @mc.identity_filter id
+end
+
+When /I call MCollective/ do
+  @out = @mc.send(@action, @arguments)
+end
+
+Then /I should get an hexadecimal jobid/ do
+  @jobid = @out.first.results[:data][:jobid] 
+  @jobid.should =~ /^[a-f0-9]+$/
+end
+
+Then /I should get a task in one of those states :/ do |tlist|
+  regexp = tlist.raw.flatten.join(sep='|')
+  @out = @mc.send('query', :jobid=>@jobid, :output=>'yes')
+  state = @out.first.results[:data][:state]
+  state.should =~ /#{regexp}/
+end
+
+Then /the StatusMsg is ([A-z_]*)/ do |statusmsg|
+  @out = @mc.send(@action, @arguments)
+  statusmsg.should == @out.first.results[:statusmsg]
+end
+
+Then /I should get a good task result within (\d+) seconds/ do |delay|
+  sleep delay.to_i
+  @out = @mc.send('query', :jobid=>@jobid, :output=>'yes')
+  state = @out.first.results[:data][:state]
+  statuscode = @out.first.results[:data][:statuscode]
+  state.should =~ /finished/
+  statuscode.should == 0
+end
+
+
